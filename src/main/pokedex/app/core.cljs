ba1caea91156c8ed26b78097c1fa3e00c1a6ef21
Developer <daniel.her@onja.org>
2022-09-13 11:40:46 +0300

    Capitalize the initial character of the card text

--- src/main/pokedex/app/core.cljs
+++ src/main/pokedex/app/core.cljs
@@ -3,11 +3,15 @@
             [reagent.core :as r])
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs-http.client :as http]
-            [cljs.core.async :refer [<!]]))
+            [cljs.core.async :refer [<!]])
+  (:require [goog.string :as gstring]))
 
 (def input-value (r/atom nil))
 
 (def data (r/atom nil))
+
+(defn title-case [name]
+  (gstring/capitalize name))
 
 (defn get-pokemon [name]
   (go (<! (http/get (str "https://pokeapi.co/api/v2/pokemon/" name) {:with-credentials? false
@@ -27,7 +31,6 @@
 (defn search-form []
   (let [val (r/atom "")]
     (defn on-submit-value [e] (.preventDefault e)
-      ;; (println (get @data :types))
       (reset! input-value @val)
       (read-response (get-pokemon @val)))
     (fn []
@@ -41,7 +44,7 @@
     [:div.card
 
      [:div
-      [:h3 (:name @data)]
+      [:h3 (title-case (:name @data))]
       [:select
        [:option "Default"]
        [:option "3D"]]]
@@ -50,14 +53,14 @@
 
      [:ul.type-list
       (for [type (get @data :types)]
-        ^{:key (get type :slot)}  [:li (get (get type :type) :name)])]]]])
+        ^{:key (get type :slot)}  [:li (title-case (get (get type :type) :name))])]]]])
 
 (defn pokedex []
   [:div.container
    [:header
     [:h1 "Pokedex"]]
-   [search-form] 
-   (if (nil? (:name @data)) "" [card])])
+   [search-form]
+   (when-not (nil? (:name @data)) [card])])
 
 (defn render []
   (rdom/render [pokedex] (.getElementById js/document "root")))
